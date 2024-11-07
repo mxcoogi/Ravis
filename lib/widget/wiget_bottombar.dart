2811117846aaa0d5609b946a99be602ec25776d6
mxcoogi <mxcoogi@gmail.com>
2024-11-07 19:51:12 +0900

    하단바 수정

--- lib/widget/wiget_bottombar.dart
+++ lib/widget/wiget_bottombar.dart
@@ -9,59 +9,62 @@ class CustomBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: selectedIndex,
-      onTap: onItemTapped,
-      type: BottomNavigationBarType.fixed,
-      items: [
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/homeIcon.svg',
-            width: 24,
-            height: 24,
-            color: Color(0xFF666E79),
+    return Container(
+      height: 100, // 원하는 높이로 설정
+      child: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/homeIcon.svg',
+              width: 30,
+              height: 30,
+              color: Color(0xFF666E79),
+            ),
+            label: '홈',
           ),
-          label: '홈',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/dangerIcon.svg',
-            width: 24,
-            height: 24,
-            color: Color(0xFF666E79),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/dangerIcon.svg',
+              width: 30,
+              height: 30,
+              color: Color(0xFF666E79),
+            ),
+            label: '위험지역',
           ),
-          label: '위험지역',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/copeIcon.svg',
-            width: 24,
-            height: 24,
-            color: Color(0xFF666E79),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/copeIcon.svg',
+              width: 30,
+              height: 30,
+              color: Color(0xFF666E79),
+            ),
+            label: '대처방법',
           ),
-          label: '대처방법',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/contactIcon.svg',
-            width: 24,
-            height: 24,
-            color: Color(0xFF666E79),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/contactIcon.svg',
+              width: 30,
+              height: 30,
+              color: Color(0xFF666E79),
+            ),
+            label: '비상 연락',
           ),
-          label: '비상 연락',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/myinfoIcon.svg',
-            width: 24,
-            height: 24,
-            color: Color(0xFF666E79),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/myinfoIcon.svg',
+              width: 30,
+              height: 30,
+              color: Color(0xFF666E79),
+            ),
+            label: '마이페이지',
           ),
-          label: '마이페이지',
-        ),
-      ],
-      selectedItemColor: Color(0xFF666E79),
-      unselectedItemColor: Color(0xFF666E79),
+        ],
+        selectedItemColor: Color(0xFF666E79),
+        unselectedItemColor: Color(0xFF666E79),
+      ),
     );
   }
 }
